48a7e77195ebd75f2570b97db2e27610f867a39b
Gaurav-Khore <28gauravkhore@gmail.com>
2025-04-06 20:04:25 +0530

    Added the nft

--- programs/mint_nfts/src/lib.rs
+++ programs/mint_nfts/src/lib.rs
@@ -1,12 +1,12 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{associated_token::AssociatedToken, metadata::Metadata, token_interface::{Mint, TokenAccount, TokenInterface,MintTo}};
+use anchor_spl::{associated_token::AssociatedToken, metadata::Metadata, token::{self, Token}, token_interface::{Mint, MintTo, TokenAccount, TokenInterface}};
 
 declare_id!("2KVpzmgNNyhL2qnfists6uxjsAytJdxNdUaBEML77rBU");
 
 #[program]
 pub mod mint_nfts {
 
-    use anchor_spl::{metadata::{create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3}, token_interface};
+    use anchor_spl::{metadata::{create_master_edition_v3, create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMasterEditionV3, CreateMetadataAccountsV3}, token_interface};
 
     use super::*;
 
@@ -101,6 +101,76 @@ pub mod mint_nfts {
         token_interface::transfer_checked(cpi_context, amount, decimals)?;
         
         msg!("Transafer successfully...");
+        Ok(())
+    }
+
+
+    pub fn mint_nfts(ctx: Context<MintNFT>,nft_name: String, nft_symbol: String, nft_uri: String) -> Result<()> {
+        
+        msg!("Minting the token");
+
+        token_interface::mint_to(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo { 
+                    mint: ctx.accounts.mint_account.to_account_info(), 
+                    to: ctx.accounts.associated_token_account.to_account_info(), 
+                    authority: ctx.accounts.payer.to_account_info() } 
+            ), 
+            1
+        )?;
+        
+
+        msg!("Minting token Successful");
+        msg!("Creating Metadata Account");
+
+        create_metadata_accounts_v3(
+            CpiContext::new(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    mint_authority: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.payer.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info() 
+                } 
+            ), 
+            DataV2 { 
+                name: nft_name, 
+                symbol: nft_symbol, 
+                uri: nft_uri, 
+                seller_fee_basis_points: 0, 
+                creators: None, 
+                collection: None, 
+                uses: None }, 
+                false, 
+                true, 
+                None
+            )?;
+
+            msg!("Metadata Account created Successfully");
+            msg!("Creating Edition Account");
+
+            create_master_edition_v3(
+                CpiContext::new(
+                    ctx.accounts.token_metadata_program.to_account_info(),
+                    CreateMasterEditionV3 {
+                        edition: ctx.accounts.edition_account.to_account_info(),
+                        metadata: ctx.accounts.metadata_account.to_account_info(),
+                        mint: ctx.accounts.mint_account.to_account_info(),
+                        mint_authority: ctx.accounts.payer.to_account_info(),
+                        payer: ctx.accounts.payer.to_account_info(),
+                        update_authority: ctx.accounts.payer.to_account_info(),
+                        token_program: ctx.accounts.token_program.to_account_info(),
+                        rent: ctx.accounts.rent.to_account_info(),
+                        system_program: ctx.accounts.system_program.to_account_info()
+                    }
+                ),
+                None // max supply 
+            )?;
+
         Ok(())
     }
 }
@@ -234,4 +304,56 @@ pub struct TransferToken<'info> {
     )]
     pub receiver_token_account : InterfaceAccount<'info,TokenAccount>,
     pub token_program: Interface<'info,TokenInterface>
+}
+
+
+// create NFT 
+#[derive(Accounts)]
+pub struct MintNFT<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// CHECK: Validate Address by deriving PDA
+    #[account(
+        mut,
+        seeds = [b"metadata",token_metadata_program.key().as_ref(),mint_account.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key()
+    )]
+    pub metadata_account: UncheckedAccount<'info>,
+
+    /// CHECK: Validate Address by deriving PDA
+    #[account(
+        mut,
+        seeds = [b"metadata",token_metadata_program.key().as_ref(),mint_account.key().as_ref(),b"edition"],
+        bump,
+        seeds::program = token_metadata_program 
+    )]
+    pub edition_account : UncheckedAccount<'info>,
+
+    // Create a mint account
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = payer.key(),
+        mint::freeze_authority = payer.key()
+    )]
+    pub mint_account : Account<'info,token::Mint>,
+
+    // Create associated token account
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = payer,
+    )]
+    pub associated_token_account :Account<'info,token::TokenAccount>,
+
+    pub token_program: Program<'info,Token>,
+    pub token_metadata_program: Program<'info,Metadata>,
+    pub associated_token_program: Program<'info,AssociatedToken>,
+    pub system_program: Program<'info,System>,
+    pub rent: Sysvar<'info,Rent>
+
 }
